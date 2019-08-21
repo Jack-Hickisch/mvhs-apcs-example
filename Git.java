6967418a4e58d5b315c1ead753839aaad652cc35
Jack Hickisch <jackhickisch@gmail.com>
2019-08-21 12:27:56 -0600

    About Padjen's video gmae

--- Git.java
+++ Git.java
@@ -6,5 +6,6 @@ public class Git
         System.out.println("Hi Mom!");
         System.out.println("APCSA");
         System.out.println("I learn code in 5th period");
+        System.out.println("when Mr. Padjen made his videogame he had 1000s of commits");
     }
 }
